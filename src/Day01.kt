d0e3f13918b931f3e83db1795cf17d834fc32167
Joel Cantoral <cs.joel.09@gmail.com>
2022-12-01 14:12:02 -0600

    add part 2 day 1
    - add second part
    - clean code
    - add helper buildSumArray fun
    - update checks

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,30 +1,51 @@
 import java.lang.Double.max
+import java.lang.Double.sum
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        var max = 0.0;
-        var currSum = 0.0;
+    fun buildSumArray(input: List<String>) : List<Int> {
+        val sums = ArrayList<Int>()
+        var currSum = 0;
         for (food in input) {
             if (food == "") {
-                max = max(max, currSum);
-                currSum = 0.0;
+                sums.add(currSum);
+                currSum = 0;
             } else {
-                currSum += food.toDouble();
+                currSum += food.toInt()
             }
         }
-        return max.toInt()
+        return sums
     }
 
+    /**
+     * Return max sum from array
+     */
+    fun part1(input: List<String>): Int {
+        var max = 0;
+        val sums = buildSumArray(input)
+        for (sum in sums){
+            if (sum > max){
+                max = sum
+            }
+        }
+        return max
+    }
+
+    /**
+     * Get 3 greatest values from array
+     */
     fun part2(input: List<String>): Int {
-        return input.size
+        val sums =  ArrayList<Int>(buildSumArray(input))
+        sums.sort()
+        return sums[sums.size-1] + sums[sums.size-2] + sums[sums.size-3]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
+    check(part2(testInput) == 41000)
 
     val input = readInput("Day01")
     check(part1(input) == 67658)
-    
-    println(part2(input))
+    check(part2(input) == 200158)
+
 }
