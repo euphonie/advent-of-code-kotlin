a26fe00a23b06449084d5f2b8122a6c7963a021e
Joel Cantoral <cs.joel.09@gmail.com>
2022-12-07 11:35:15 -0600

    day 3 part 2
    find matching badges in triplets

--- src/Day03.kt
+++ src/Day03.kt
@@ -18,10 +18,26 @@ fun main() {
         return input.sumOf { rucksack -> countMatchingTypes(rucksack) }
     }
 
+    fun getBadgePriority(group: List<String>) : Int {
+        val matchingType = group[0].filter {
+            it in group[1] && it in group[2]
+        }.toSet()
+        return matchingType.sumOf { c -> getCharValue(c) }
+    }
+
+    fun part2(input: List<String>) : Int {
+        var badgePriorities = 0
+        for (i in input.indices step 3) {
+            badgePriorities += getBadgePriority(input.slice(i..i+2))
+        }
+        return badgePriorities
+    }
 
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     check(part1(input) == 7763)
+    check(part2(input) == 2569)
 }
