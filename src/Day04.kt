d641eb342f56f743941e1e20f0c27b365ca6c8b8
Joel Cantoral <cs.joel.09@gmail.com>
2022-12-07 13:22:44 -0600

    day4 part 2
    add different strategies depending
    on full range or partial range validation

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,16 +1,54 @@
+interface OverlapValidator {
+    fun checks(range: IntRange, otherRange: IntRange) : Boolean
+}
+class FullOverlapValidator: OverlapValidator {
+    override fun checks(range: IntRange, otherRange: IntRange): Boolean {
+        val firstInSecond =
+            range.first in otherRange  &&
+                    range.last in otherRange
+
+        val secondInFirst =
+            otherRange.first in range.first..range.last  &&
+                    otherRange.last in range.first..range.last
+
+        return firstInSecond || secondInFirst
+    }
+}
+
+class PartialOverlapValidator : OverlapValidator {
+    override fun checks(range: IntRange, otherRange: IntRange): Boolean {
+        val firstInSecond =
+            range.first in otherRange  ||
+                    range.last in otherRange
+
+        val secondInFirst =
+            otherRange.first in range.first..range.last  ||
+                    otherRange.last in range.first..range.last
+
+        return firstInSecond || secondInFirst
+    }
+}
+
 fun main() {
 
     fun existsFullIntersection(assignment: String) : Int {
         val ranges = assignment.split(",", "-")
+        val validator = FullOverlapValidator()
+        val isContained = validator.checks(
+            ranges[0].toInt()..ranges[1].toInt(),
+            ranges[2].toInt()..ranges[3].toInt())
 
-        val firstInSecond =
-            ranges[0].toInt() in ranges[2].toInt()..ranges[3].toInt()  &&
-                    ranges[1].toInt() in ranges[2].toInt()..ranges[3].toInt()
+        return if (isContained) 1 else 0
+    }
 
-        val secondInFirst =
-            ranges[2].toInt() in ranges[0].toInt()..ranges[1].toInt()  &&
-                    ranges[3].toInt() in ranges[0].toInt()..ranges[1].toInt()
-        return if (firstInSecond || secondInFirst ) 1 else 0
+    fun existsPartialIntersection(assignment: String) : Int {
+        val ranges = assignment.split(",", "-")
+        val validator = PartialOverlapValidator()
+        val isContained = validator.checks(
+            ranges[0].toInt()..ranges[1].toInt(),
+            ranges[2].toInt()..ranges[3].toInt())
+
+        return if (isContained) 1 else 0
     }
 
 
@@ -19,13 +57,15 @@ fun main() {
     }
 
     fun part2(input: List<String>) : Int {
-        return 2
+        return input.sumOf { existsPartialIntersection(it) }
     }
 
 
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     check(part1(input) == 475)
+    check(part2(input) == 825)
 }
